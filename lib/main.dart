f455a810d4e584c1eb33d52c241e3720abc475a3
ohanz <Chiaguguadaniel@gmail.com>
2023-09-16 10:59:42 +0100

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
 
         ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: true, // to be removed
       ),
       home: const SplashScreen()
     );
